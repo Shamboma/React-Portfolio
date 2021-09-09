bc7826375caaace025210aae79e1b54fcb8d6e46
Austin <onei0205@algonquinlive.com>
2021-09-09 15:03:49 -0400

    Added Home page
    -Svg DownArrow added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import downArrow from "../assests/DownArrow.svg";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <h1>Home</h1>
+      <h1>Austin O'Neill</h1>
+      <h1>Professional</h1>
+      <h1>Full Stack Developer</h1>
+      <Link to={"/projects"}>
+        <button>
+          <img src={downArrow} alt={downArrow} />
+        </button>
+      </Link>
     </>
   );
 };
