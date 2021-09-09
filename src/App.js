4a3fa01197efcb69afb93bed6f0f24538d6e94f2
Austin <onei0205@algonquinlive.com>
2021-09-09 14:33:26 -0400

    Added Router & Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={"/"}>
+          <Home />
+        </Route>
+        <Route path={"/projects"}>
+          <Projects />
+        </Route>
+        <Route path={"/about"}>
+          <About />
+        </Route>
+        <Route path={"/contact"}>
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
