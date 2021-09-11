c8d1b3eebfe5ceee1ebc6195491fd7069666d64a
Austin <onei0205@algonquinlive.com>
2021-09-11 17:45:57 -0400

    Added About page
    -This marks the end of js for now, time for some css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,25 @@
+import NavSide from "../components/NavSide";
+import { useState } from "react";
+import Heading from "../components/Heading";
+import downArrow from "../assests/ArrowDownFancy.png";
+
 const About = () => {
+  const [showPersonal, setShowPersonal] = useState(false);
+  const changeHandler = () => {
+    if (showPersonal === false) {
+      setShowPersonal(true);
+    } else setShowPersonal(false);
+  };
   return (
     <>
-      <h1>About</h1>
+      <img src={"Resume.png"} alt={"Resume.png"} />
+      {showPersonal ? (
+        <Heading title={"Personal About"} desc={"DESC"} />
+      ) : (
+        <Heading title={"Professional About"} desc={"DESC"} />
+      )}
+      <img onClick={changeHandler} src={downArrow} alt={downArrow} />
+      <NavSide up={"/projects"} down={"/contact"} />
     </>
   );
 };
